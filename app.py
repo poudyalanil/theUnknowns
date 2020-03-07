13bf7e661333568cb171c55f3a30cd9dd3bbe403
poudyalanil <anilrunjakote@gmail.com>
2020-03-07 20:28:16 +0545

    Interface error solved

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,  url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 app  = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///tuks.db'
@@ -11,8 +11,8 @@ class portfoliodtl(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable = False)
     company = db.Column(db.String(120), nullable = True)
-    year_completed = db.Column(db.Integer)
-    info = db.Column(db.String(500), nullable = False)
+    year_completed = db.Column(db.String(120), nullable = True)
+    info = db.Column(db.String(200), nullable = False)
     image = db.Column(db.String(2083), nullable = False)
     link = db.Column(db.String(2083))
 
@@ -38,25 +38,27 @@ def portfolio():
         name = request.form['name']
         year = request.form['year']
         images = request.form['image']
-        infos =request.form['info'],
+        infos =request.form['info']
         links =request.form['link']
         
         newPortfolio = portfoliodtl(
-            title=titles,
-            company=name,
-            year_completed=year,
+            title=str(titles),
+            company=str(name),
+            year_completed=str(year),
             info=infos,
-            image=images,
-            link = links
+            image=str(images),
+            link = str(links),
 
         )
+        
         try:
             db.session.add(newPortfolio)
+                
             db.session.commit()
-             
             return redirect('/')
         except:
-            return "Couldnot add a portfolio "
+            return "Couldnot Add :("
+            
     else:
         portfolio = portfoliodtl.query.all()
         return render_template('/addPortfolio.html',p = portfolio)
@@ -80,24 +82,26 @@ def update(id):
 
 
     if request.method =='POST':
-        pl.titles = request.form['title']
-        pl.name = request.form['name']
-        pl.year = request.form['year']
-        pl.images = request.form['image']
+        pl.titles = str(request.form['title'])
+        pl.name = str(request.form['name'])
+        pl.year = str(request.form['year'])
+        pl.images = str(request.form['image'])
         pl.infos =request.form['info']
-        pl.link =request.form['link']
+        pl.link =str(request.form['link'])
 
         
+        
         try:
-            
+
             db.session.commit()
-             
             return redirect('/')
         except:
-            return "Couldnot update a portfolio "
+            return "Couldnot Update :("
+        
+        
     else:
         portfolio = portfoliodtl.query.all()
         return render_template('/editPortfolio.html',p = pl)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=False)
